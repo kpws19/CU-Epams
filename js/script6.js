b52b3bd39af69f5e63b12c97b5d3fe1af6f147bb
GauravWalia19 <gauravwalia019@gmail.com>
2018-01-06 00:25:29 +0530

    solving homepage.html

--- js/script6.js
+++ js/script6.js
@@ -1,4 +1,5 @@
 //javascript for personnal info.
+/*
 function openCity(evt, cityName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -12,6 +13,29 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     document.getElementById('hide').style.display="none";
     document.getElementById('hides').style.display="none";
+    document.getElementById('gaurav').style.display="block";
+    evt.currentTarget.className += " active";
+}
+*/
+function openCity() {
+    /*
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    */
+    document.getElementById('London').style.display = "block";
+    document.getElementById('hide').style.display="none";
+    document.getElementById('hides').style.display="none";
+    document.getElementById('gaurav').style.display="block";
+    document.getElementsByClassName('wrapper col4')[0].style.display="none";
+    document.getElementsByClassName('wrapper col5')[0].style.display="none";
     evt.currentTarget.className += " active";
 }
 // Get the element with id="defaultOpen" and click on it
@@ -21,7 +45,7 @@ function personalcontent()
 {
   document.getElementById('data').style.display = "block";
 }
-/*##########################################################*/
+/*########################FOR MENU BAR IN PERSONNAL INFO##################################*/
 function openDetail(evt, cityName) {
     var i, toffeecontent, toffee;
     toffeecontent = document.getElementsByClassName("toffeecontent");
