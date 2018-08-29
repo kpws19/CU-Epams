efe711cdb2577e7992730ee028c638843976ec9b
GauravWalia19 <gauravwalia019@gmail.com>
2018-08-29 11:42:41 +0530

    added more foolders and php

---
+++ php/signup.php
@@ -0,0 +1,9 @@
+<?php
+echo "Name: ".$_POST["username"]."<br/>";
+echo "Roll No:  ".$_POST["rollno"]."<br/>";
+echo "Email ".$_POST["emailaddress"]."<br/>";
+echo "Stream    ".$_POST["stream"]."<br/>";
+echo "Date  ".$_POST["entered-date"]."<br/>";
+echo "Gender    ".$_POST["gender"]."<br/>";
+echo "Phone Number  ".$_POST["phoneno"]."<br/>";
+?>
